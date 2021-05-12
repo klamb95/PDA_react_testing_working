f264a9c11200bf4a2f5bbe7ab927f32cc320defb
klamb95 <klamb1995@gmail.com>
2021-05-12 18:51:45 +0100

    deal with a range of numbers

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -33,6 +33,52 @@ describe("Calculator", () => {
   })
 
 
+  it('should deal with positive numbers', ()=>{
+    cy.get('#number2').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '4');
+  })
+
+  it('should deal with negative numbers', ()=>{
+    cy.get('#number2').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-1');
+  });
+
+
+  it('should deal with decimal numbers', ()=>{
+    cy.get('#number6').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.5');
+  });
+
+
+
+  it('should deal with large numbers', ()=>{
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '50000');
+  });
+
+  it('should deal with divide by zero')
+
+
+
+  
+
   
 
 })
