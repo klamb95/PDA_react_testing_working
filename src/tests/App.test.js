babf29dab822c679dc86485cc9de118e3941aef4
klamb95 <klamb1995@gmail.com>
2021-05-12 18:01:57 +0100

    add two numbers passed

---
+++ src/tests/App.test.js
@@ -0,0 +1,29 @@
+import { fireEvent, render } from '@testing-library/react';
+import App from '../App';
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = render(<App/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.getByTestId('number4');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button4);
+    expect(runningTotal).toHaveTextContent('4');
+  })
+
+  it('should add two numbers together', () => {
+    const button1 = container.getByTestId('number1');
+    const add = container.getByTestId('add');
+    const equals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button1);
+    fireEvent.click(equals);
+    expect(runningTotal).toHaveTextContent('2');
+    })
+})
+
